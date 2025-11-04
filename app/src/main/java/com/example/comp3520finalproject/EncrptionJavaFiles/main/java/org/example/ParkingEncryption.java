3598323a255d2811bd4835249a10c0363a80552c
agapelogos4 <72779687+agapelogos4@users.noreply.github.com>
2025-11-03 20:54:46 -0800

    EncryptionFolder
    Shifted the Encryption Java files to the master branch which will soon be the main branch

---
+++ app/src/main/java/com/example/comp3520finalproject/EncrptionJavaFiles/main/java/org/example/ParkingEncryption.java
@@ -0,0 +1,109 @@
+package org.example;
+
+/**
+ * This class creates some encryption methods for the Parking App
+ */
+
+public class ParkingEncryption
+{
+    // This is the fixed shift amount for simplicity instead of choosing the shift each time
+    private static final int SHIFT = 5;
+    /**
+     * The Caesar Cipher will shift the inputs by the determined amount
+     * It works for both alphabetical and numerical characters
+     * Non-alphanumerical characters will stay the same
+     *
+     * @param input: The input string that will be encrypted
+     * @return The transformed input after the ciphering (null if the input was null) returned as a string
+     */
+    public static String caesarCipher(String input)
+    {
+        if (input == null)
+        {
+            return null;
+        }
+
+        String result = "";
+
+        for (char c : input.toCharArray())
+        {
+            if (c >= 'A' && c <= 'Z')
+            {
+                char shiftedChar = (char) (c + SHIFT);
+                if (shiftedChar > 'Z')
+                {
+                    shiftedChar -= 26;
+                }
+                result += shiftedChar;
+            }
+
+            else if (c >= 'a' && c <= 'z')
+            {
+                char shiftedChar = (char) (c + SHIFT);
+                if (shiftedChar > 'z')
+                {
+                    shiftedChar -= 26;
+                }
+                result += shiftedChar;
+            }
+
+            else if (c >= '0' && c <= '9')
+            {
+                char shiftedChar = (char) (c + SHIFT);
+                if (shiftedChar > '9')
+                {
+                    shiftedChar -= 10;
+                }
+                result += shiftedChar;
+            }
+            else
+            {
+                result += c;
+            }
+        }
+        return result;
+    }
+
+    /**
+     * Decryption method for the Caesar Cipher
+     *
+     * @param input: This is the encrypted message (will return null if it is Null)
+     * @return The decrypted input is returned as a string
+     */
+    public static String caesarDecipher(String input)
+    {
+            if (input == null)
+            {
+                return null;
+            }
+
+            String result = "";
+
+            for (char c : input.toCharArray())
+            {
+                if (c >= 'A' && c <= 'Z') {
+                    char shiftedChar = (char)(c - SHIFT);
+                    if (shiftedChar < 'A')
+                    {
+                        shiftedChar += 26;
+                    }
+                    result += shiftedChar;
+
+                } else if (c >= 'a' && c <= 'z') {
+                    char shiftedChar = (char)(c - SHIFT);
+                    if (shiftedChar < 'a') shiftedChar += 26;
+                    result += shiftedChar;
+
+                } else if (c >= '0' && c <= '9') {
+                    char shiftedChar = (char)(c - SHIFT);
+                    if (shiftedChar < '0') shiftedChar += 10;
+                    result += shiftedChar;
+
+                } else {
+                    result += c;
+                }
+            }
+
+            return result;
+    }
+}
