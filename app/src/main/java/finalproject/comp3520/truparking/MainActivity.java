13ef7c9f279114822814da0b21ddaebca4df81d2
Chris Murphy <christianmurphy@hotmail.ca>
2025-11-11 11:16:20 -0800

    added login button to topnav, eventually this button will swap based on whether or not the user is logged in.

--- app/src/main/java/finalproject/comp3520/truparking/MainActivity.java
+++ app/src/main/java/finalproject/comp3520/truparking/MainActivity.java
@@ -19,7 +19,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Button lotNButton = findViewById(R.id.LotNButton);
-        ImageButton loginButton = findViewById(R.id.loginButton);
+        ImageButton loginButton = findViewById(R.id.loginLogoutButton);
 
 
         androidx.appcompat.widget.Toolbar toolbar = findViewById(R.id.top_toolbar);
